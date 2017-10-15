f4e17f1bd82436c0783116726cb75718029e42f4
AnirudhDevulapalli <danirudh2017@gmail.com>
2017-10-14 22:19:09 -0700

    Create svcc.go

---
+++ SVCC_2017/svcc.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"io"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":9090", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request){
+	io.WriteString(w, "Hello SVCC from Paypal")
+}
